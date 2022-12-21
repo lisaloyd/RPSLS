9c0aec5745d9cac7baf88655088703d8a23b0920
austinw298 <austinw298@gmail.com>
2022-12-21 13:31:26 -0700

    added player class

---
+++ player.py
@@ -0,0 +1,13 @@
+#Rock crushes Scissors
+#Scissors cuts Paper 
+#Paper covers Rock
+#Rock crushes Lizard
+#Lizard poisons Spock
+#Spock smashes Scissors
+#Scissors decapitates Lizard
+#Lizard eats Paper
+#Paper disproves Spock
+#Spock vaporizes Rock
+class player:
+    def __init__(self) -> None:
+        pass
