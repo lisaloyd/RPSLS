5ce8438daf65db97694763656bc90d7ca037be73
austinw298 <austinw298@gmail.com>
2022-12-22 10:37:52 -0700

    combining files to repository

--- player.py
+++ player.py
@@ -8,6 +8,32 @@
 #Lizard eats Paper
 #Paper disproves Spock
 #Spock vaporizes Rock
-class player:
-    def __init__(self) -> None:
-        pass
+
+# 1 is rock
+# 2 is paper
+# 3 is scissors 
+# 4 is lizard
+# 5 is spock
+
+gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+
+
+class Player:
+    def __init__(self):
+        self = self
+
+    def gesture(self, gesture):    
+        self.gesture1 = "rock"
+        self.paper = 2
+        self.scissors = 3
+        self.lizard = 4
+        self.spock = 5
+
+# class Player:
+#     def __init__(self):
+#         self.rock = "Rock"
+#         self.paper = "Paper"
+#         self.scissors = "Scissors"
+#         self.lizard = "Lizard"
+#         self.spock = "Spock"
+
