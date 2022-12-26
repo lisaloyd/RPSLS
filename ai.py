0c6ec7ac3ce5dc62ff488ce28216d20ad43d0eb3
unknown <greateducation123@yahoo.com>
2022-12-26 09:08:26 -0600

    AI Class updated

--- ai.py
+++ ai.py
@@ -1,14 +1,13 @@
 from player import Player
-
+import random
 
 class AI(Player):
-    def __init__(self, name):
-        super(). __init__()
-        self.name = name
-        self.score = 0
-
-    def display_choices(self):
-        self.display_choices = str(random.randint(1,5))
+    def __init__(self, name) -> None:
+        super().__init__(name)
         
-   
+
+    def computer_choice(self):
+        self.computer_choice = random.randint(0, 4)
+        gesture = self.gesture_list
+        print (f"{self.name} picked {gesture[int(self.computer_choice)]}")
 #sleep
