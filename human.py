3ec71396d3c2bf0f730df56ae9ace06a913d8f60
austinw298 <austinw298@gmail.com>
2022-12-26 09:39:58 -0700

    added to human class and updating to current

--- human.py
+++ human.py
@@ -8,6 +8,7 @@ class Human(Player):
     def display_choices(self):
         # for gesture in self.gesture_list:
         #     print(gesture)
+        print()
         for count in range(len(self.gesture_list)):
             gesture = self.gesture_list[count]
             print(count + 1, gesture)
@@ -15,11 +16,18 @@ class Human(Player):
 
    
     def choose_gesture(self):
-        #
-        #display gestures to user
+        print()
+        self.choose = input("Choose a number from above list ")
+        print()
+        if self.choose == [1, 2, 3, 4, 5]:
+            print()
+            print(f"Player has chosen {self.choose}!")
+        else:
+            print("That is not an option")
+
+
+    
         #ask user for gesture from list above, store input into variable
         #compare from list
         #or
-        #use user input to index gesture list
-        pass
-
+        #use user input to index in gesture list
